9c3badc945961969a981036660a0ce5964d23749
Alessandro <alessandropds55@gmail.com>
2022-10-11 20:12:12 -0300

    Notificando o cliente por email

--- workercompras/src/main/java/com/workercompras/consumer/Consumer.java
+++ workercompras/src/main/java/com/workercompras/consumer/Consumer.java
@@ -2,7 +2,9 @@ package com.workercompras.consumer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.workercompras.model.Pedido;
+import com.workercompras.service.EmailService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import lombok.var;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.io.Serializable;
 
+@Slf4j
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Component
 public class Consumer implements Serializable {
@@ -20,12 +23,14 @@ public class Consumer implements Serializable {
     private static final long serialVersionUID = 1L;
 
     private final ObjectMapper mapper;
+    private final EmailService emailService;
 
     // ESCUTANDO MENSAGEM
     @RabbitListener(queues = {"${queue.name}"})
     public void consumer(@Payload Message message) throws IOException {
         var pedido = mapper.readValue(message.getBody(), Pedido.class);
         System.out.println("Mensagem recebida: " + pedido);
+        emailService.notificarCliente("alessandropds55@gmail.com");
     }
 
 }
