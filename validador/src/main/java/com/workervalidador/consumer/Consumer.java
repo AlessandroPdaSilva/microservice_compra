979a4e75f497902513069726bfa92a4e371804f4
Alessandro <alessandropds55@gmail.com>
2022-10-16 18:09:30 -0300

    Consumindo a fila compra pendente pelo Validador

---
+++ validador/src/main/java/com/workervalidador/consumer/Consumer.java
@@ -0,0 +1,30 @@
+package com.workervalidador.consumer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.workervalidador.model.Pedido;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.var;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Component
+public class Consumer {
+
+    private final ObjectMapper mapper;
+
+    @RabbitListener(queues = {"${queue.name}"})
+    public void consumer(@Payload Message message) throws IOException {
+        var pedido = mapper.readValue(message.getBody(), Pedido.class);
+        log.info("Pedido recebido no Validador: {}", pedido);
+    }
+
+}
